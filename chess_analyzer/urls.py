1cb7fe8d2a696832b4f2d80d5a6d099d7e8bd968
Wonders243 <merveilleskatabisomwe@gmail.com>
2025-06-04 06:56:46 +0200

    Update urls.py

--- chess_analyzer/urls.py
+++ chess_analyzer/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('analysis.urls')),  # ← la page d'accueil affiche l'analyse
 ]
